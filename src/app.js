f35cb2007ba69195414fa7f185dbfe2730958d72
Dhaval <joshidhaval2002@gmail.com>
2025-06-18 13:28:21 +0530

    did add,read and delete DB operation using mongoose

--- src/app.js
+++ src/app.js
@@ -7,22 +7,57 @@ const User = require("./Models/user");
 //   res.send("Only admin route");
 // });
 
+app.use(express.json()); //middleware to convert req coming in JSON to HS object so express server could understand+
+
 app.post("/signup", async (req, res) => {
   //created an instance of the MODEL (User)
-  const user = new User({
-    firstName: "JAIDEN",
-    password: "jaiden@123",
-    lastName: "Joshi",
-    emailId: "jaiden@gmai.com",
-  });
+  // console.log(req.body);
+  const user = new User(req.body);
 
   try {
     await user.save(); //this return promise that why asyn await used
-    res.send("User data added Successfully!!!!!!");
+    res.send("dynamic User data added Successfully!!!!!!");
   } catch (err) {
-    res.status(500).send("Data not added");
+    res.status(400).send("Data not added");
   }
 });
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("NO user exist");
+  }
+});
+
+// Feed api get all user from DATABASE  find(takes filter if we did {} this brackets it will return all data in db)
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("there is NO user in feed");
+  }
+});
+
+
+//Deleting a  User from ID 
+app.delete("/user",async (req,res)=>{
+  const firstName=req.body.firstName
+  console.log(firstName)
+  try{
+    const delUser=await User.findOneAndDelete({"firstName":firstName});
+    console.log(delUser,"deluserslnjeb")
+    res.send("user deleted,Name of user:"+delUser.firstName+" "+delUser.lastName)
+    
+  }
+  catch(err){
+    res.send("error deleting user")
+  }
+})
 
 connectDB().then(() => {
   console.log("DB connected");
