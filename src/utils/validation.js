22d1b4438e86234c4911a1a6cd12a98ee69c4614
Dhaval <joshidhaval2002@gmail.com>
2025-06-23 15:52:28 +0530

    validation code of edit profile data

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,12 +1,32 @@
-const validation=(req)=>{
-    const { firstName, lastName, emailId, password } = req.body;
+//TODO validation of USer auth create a middleware for authenticating user to access api and then refactoring jwt and bcrypt inside user.js(UserSchema)
 
-    if(!firstName || !lastName){
-        throw new Error("Enter the Name please")
-    }
-    
-}
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
 
-module.exports={
-    validation
-}
+  if (!firstName || !lastName) {
+    throw new Error("Enter the Name please");
+  }
+};
+
+const validateEditProfileData = (req) => {
+  const allowedEditFIelds = [
+    "firstName",
+    "lastName",
+    "photoUrl",
+    "about",
+    "skills",
+    "age",
+    "gender",
+  ];
+
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFIelds.includes(field)
+  );
+  //return a boolean
+  return isEditAllowed;
+};
+
+module.exports = {
+  validateSignUpData,
+  validateEditProfileData,
+};
