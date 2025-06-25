4e66dc005212e1d5a2c9620291caa58c96def045
Dhaval <joshidhaval2002@gmail.com>
2025-06-25 16:12:15 +0530

    created an api for user my conenctions

--- src/routes/user.js
+++ src/routes/user.js
@@ -12,10 +12,36 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
   const connectionRequest = await ConnectionRequestModel.find({
     toUserId: loggedInUser._id,
     status: "interested",
+  }).populate("fromUserId", "firstName lastName age photoUrl about skills ");
+  //   }).populate("fromUserId",["firstName" "lastName"]);
+
+  res.send({
+    message: "Here is the requests sent to you by following people",
+    data: connectionRequest,
+  });
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  const loggedInUser = req.userData;
+
+  const acceptedConnectionRequests = await ConnectionRequestModel.find({
+    $or: [{ fromUserId: loggedInUser._id,status: "accepted" }, { toUserId: loggedInUser._id ,status: "accepted"}],
+    
+  })
+    .populate("fromUserId", "firstName lastName age skills about photoUrl")
+    .populate("toUserId", "firstName lastName age skills about photoUrl");
+
+  const sendBy = acceptedConnectionRequests.map((row) => {
+    //the acceptedCOnenctionRequest will habve data form user colelction of both form and to but i want the one whihc is not loggedin user
+    if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+      return  row.toUserId;
+    }
+    else{
+        return row.fromUserId;
+    }
   });
 
-
-  res.send({message:"Here is the requests sent to you by following people",data:connectionRequest})
+  res.send({ message: "Your conection Request", data: sendBy });
 });
 
 module.exports = userRouter;
