dc4113f55d28f713d8eff589e2ed407828df2bb2
Dhaval <joshidhaval2002@gmail.com>
2025-07-02 18:27:20 +0530

    installed  CORS and set orgin and credentials

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ const authRouter=require("./routes/auth")
 const profileRouter=require("./routes/profile")
 const requestRouter=require("./routes/request")
 const userRouter=require("./routes/user")
+const cors=require("cors")
+
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+})) //to allow frontend to req backend or to know backend that allow req form frontend
 
 app.use(express.json()); //middleware to convert req coming in JSON to JS object so express server could understand+
 app.use(cookieParser()); //middleware to read cookie otherwise guves undefined it not added this middleware
